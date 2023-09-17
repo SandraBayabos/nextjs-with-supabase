4a214057eec1626a234aa85628c23e48e924deca
Sandra Bayabos <48815451+SandraBayabos@users.noreply.github.com>
2023-09-17 22:22:58 +0800

    fe & logic: css styling for sudoku board, refactored code, added enums for game state, added function to populate solution into board

--- app/components/SudokuPuzzle.tsx
+++ app/components/SudokuPuzzle.tsx
@@ -1,86 +1,188 @@
 "use client";
 
-import React, { useState, useRef } from "react";
-import { FC } from "react";
-import {
-  calculateCellPosition,
-} from "../helpers/matrix";
+import { FC, useRef, useState } from "react";
+import { checkSudoku, getFlattenIndex } from "../helpers/matrix";
+import InputButtons from "./InputButtons";
+import Confetti from "./Confetti";
+
+enum GAME_STATE {
+  PLAYING,
+  WON,
+  LOST,
+}
 
 interface SudokuPuzzleProps {
   selectedPuzzle: string;
+  puzzleSolution: string[];
 }
 
 const SudokuPuzzle: FC<SudokuPuzzleProps> = (props) => {
+  const [activeIndex, setActiveIndex] = useState<number>();
+
   const [puzzle, setPuzzle] = useState<string[]>(
     props.selectedPuzzle.split("")
   );
+
+  const [gameState, setGameState] = useState(GAME_STATE.PLAYING);
+
   const initialPuzzle = useRef<string[]>(props.selectedPuzzle.split(""));
+  
+  const isFilled = puzzle.every((cell) => cell !== ".");
 
   const getCellValue = (boxIndex: number, cellIndex: number): string => {
-    const [row, col] = calculateCellPosition(boxIndex, cellIndex);
-    return puzzle[row * 9 + col];
+    const idx = getFlattenIndex(boxIndex, cellIndex);
+    return puzzle[idx];
   };
 
   const handleInputChange = (row: number, col: number, value: string) => {
-    const flatIndex = row * 9 + col;
-    setPuzzle((prevPuzzle) =>
-      prevPuzzle.map((n, i) => (i === flatIndex ? value : n))
+    const flatIndex = getFlattenIndex(row, col);
+
+    if (value === "" || (value.length === 1 && value >= "1" && value <= "9")) {
+      setPuzzle((prevPuzzle) =>
+        prevPuzzle.map((n, i) => (i === flatIndex ? value : n))
+      );
+    }
+  };
+
+  function validatePuzzle() {
+    const result = checkSudoku(initialPuzzle.current, puzzle);
+    if (result) {
+      setGameState(GAME_STATE.WON);
+    } else {
+      setGameState(GAME_STATE.LOST);
+    }
+  }
+
+  const GameStateDisplay = () => {
+    const loadNewPuzzle = () => {
+      window.location.reload();
+      // setGameState(GAME_STATE.PLAYING);
+    };
+    const continuePuzzle = () => {
+      setGameState(GAME_STATE.PLAYING);
+    };
+
+    const gameStateSwitch = {
+      [GAME_STATE.PLAYING]: (
+        <div className="text-4xl font-black text-red-400 tracking-wide">
+          SUDOKU
+        </div>
+      ),
+      [GAME_STATE.WON]: (
+        <>
+          <Confetti />
+          <div className="text-4xl font-black text-red-400 tracking-wide">
+            YOU WON!
+          </div>
+        </>
+      ),
+      [GAME_STATE.LOST]: (
+        <>
+          <div className="text-4xl font-black text-red-400 tracking-wide">
+            YOU LOST!
+          </div>
+          <button onClick={continuePuzzle}>Continue</button>
+          <button onClick={loadNewPuzzle}>I Give Up..</button>
+        </>
+      ),
+    };
+    return (
+      <>
+        <div className="flex flex-col">{gameStateSwitch[gameState]}</div>
+      </>
     );
   };
 
-  const isEditable = (boxIndex: number, cellIndex: number): boolean => {
-    const [row, col] = calculateCellPosition(boxIndex, cellIndex);
-    return initialPuzzle.current[row * 9 + col] === ".";
+
+  const solveForMe = () => {
+    setPuzzle(props.puzzleSolution);
   };
 
   return (
-    <div className="sudoku-board p-2 w-6/12 max-w-xl">
-      <div className="flex justify-between">
-        <h1 className="text-2xl">Sudoku</h1>{" "}
-        <div>
-          <h2>Timer</h2>
+    <>
+      <GameStateDisplay />
+      <div className="sudoku-board p-2 w-6/12 max-w-xl">
+        <div className="grid grid-cols-[1fr_35px] items-center gap-4">
+          <div>
+            <div className="flex justify-between">
+              <div>
+                <h2>Timer</h2>
+              </div>
+            </div>
+            <div className="grid grid-cols-[1fr_1fr_1fr] grid-rows-[1fr_1fr_1fr]">
+              {Array.from({ length: 9 }).map((_, boxIndex) => (
+                <div
+                  className="grid grid-cols-[1fr_1fr_1fr] grid-rows-[1fr_1fr_1fr] border-2 border-sky-200"
+                  key={boxIndex}
+                >
+                  {Array.from({ length: 9 }).map((_, cellIndex) => {
+                    const cellValue = getCellValue(boxIndex, cellIndex);
+                    const isEditable =
+                      initialPuzzle.current[
+                        getFlattenIndex(boxIndex, cellIndex)
+                      ] === ".";
+
+                    return (
+                      <input
+                        key={`${boxIndex}-${cellIndex}`}
+                        className={`cursor-pointer aspect-square border border-sky-200 w-full h-full text-center text-lg font-bold outline-none focus:border-white focus:border-4 focus:border-sky-50 transition-all duration-300 ${
+                          isEditable
+                            ? "bg-red-100 hover:bg-red-50"
+                            : "bg-transparent"
+                        }`}
+                        type="number"
+                        min="1"
+                        max="9"
+                        onFocus={() =>
+                          setActiveIndex(
+                            isEditable
+                              ? getFlattenIndex(boxIndex, cellIndex)
+                              : undefined
+                          )
+                        }
+                        value={cellValue === "." ? "" : cellValue}
+                        readOnly={!isEditable}
+                        onChange={(e) => {
+                          handleInputChange(
+                            boxIndex,
+                            cellIndex,
+                            e.target.value
+                          );
+                        }}
+                      />
+                    );
+                  })}
+                </div>
+              ))}
+            </div>
+          </div>
+          <InputButtons
+            activeIndex={activeIndex}
+            puzzle={puzzle}
+            setPuzzle={setPuzzle}
+          />
+        </div>
+        <div className="flex justify-between pt-8">
+          <button className="btn text-slate-100 bg-gradient-to-tr from-sky-300 to-sky-500 rounded-md py-2 px-3">
+            Load New Puzzle
+          </button>
+          <button
+            className={`btn text-slate-100 bg-gradient-to-tr from-sky-300 to-sky-500 rounded-md py-2 px-3 ${
+              isFilled ? "" : "opacity-50 cursor-not-allowed"
+            }`}
+            onClick={validatePuzzle}
+          >
+            Check
+          </button>
+          <button
+            className="btn text-slate-100 bg-gradient-to-tr from-sky-300 to-sky-500 rounded-md py-2 px-3"
+            onClick={solveForMe}
+          >
+            Solve For Me
+          </button>
         </div>
       </div>
-
-      <div className="grid grid-cols-[1fr_1fr_1fr] grid-rows-[1fr_1fr_1fr] gap-2 ">
-        {Array.from({ length: 9 }).map((_, boxIndex) => (
-          <div
-            className="grid grid-cols-[1fr_1fr_1fr] grid-rows-[1fr_1fr_1fr] border-2"
-            key={boxIndex}
-          >
-            {Array.from({ length: 9 }).map((_, cellIndex) => (
-              <div
-                className="p-2 border text-center font-bold"
-                key={`cell-${cellIndex}`}
-              >
-                <input
-                  className="w-full h-full text-center text-lg font-bold bg-sky-50"
-                  type="number"
-                  value={
-                    getCellValue(boxIndex, cellIndex) === "."
-                      ? ""
-                      : getCellValue(boxIndex, cellIndex)
-                  }
-                  readOnly={!isEditable(boxIndex, cellIndex)}
-                  onChange={(e) => {
-                    const [row, col] = calculateCellPosition(
-                      boxIndex,
-                      cellIndex
-                    );
-                    handleInputChange(row, col, e.target.value);
-                  }}
-                />
-              </div>
-            ))}
-          </div>
-        ))}
-      </div>
-      <div className="flex justify-between pt-8">
-        <button className="btn">Load New Puzzle</button>
-        <button className="btn">Check</button>
-        <button className="btn">Solve For Me</button>
-      </div>
-    </div>
+    </>
   );
 };
 
--- app/page.tsx
+++ app/page.tsx
@@ -11,18 +11,27 @@ export default async function Home() {
   if (data) {
     const randomIndex = Math.floor(Math.random() * data.length);
     selectedPuzzle = data[randomIndex].puzzle;
-    const solvedPuzzleArray = solveSudoku(selectedPuzzle.split(""));
-    const solvedPuzzle = solvedPuzzleArray.map(cell => cell === null ? "." : cell.toString());
+    const solvedPuzzle = solveSudoku(selectedPuzzle.split(""));
 
     return (
       <div className="w-full flex flex-col items-center">
         <SudokuPuzzle selectedPuzzle={selectedPuzzle} puzzleSolution={solvedPuzzle} />
+        {/* <SudokuPuzzle
+          selectedPuzzle={
+            "..3456789456789123789123456214365897365897214897214365531642978642978531978531642"
+          }
+          puzzleSolution={
+            "123456789456789123789123456214365897365897214897214365531642978642978531978531642".split('')
+          }
+        /> */}
       </div>
     );
   } else if (error) {
     return (
       <div className="w-full flex flex-col items-center">
-        <h1>Error: {error.message} Please contact the administrator for help.</h1>
+        <h1>
+          Error: {error.message} Please contact the administrator for help.
+        </h1>
       </div>
     );
   }
